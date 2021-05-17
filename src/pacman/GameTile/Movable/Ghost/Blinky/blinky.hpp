08341f621efbfe78606f6d74a812f3c3dcad72d2
Mircea Anton <mircea.anton99@yahoo.com>
2021-05-18 00:56:24 +0300

    Implement `Blinky` Ghost type

---
+++ src/pacman/GameTile/Movable/Ghost/Blinky/blinky.hpp
@@ -0,0 +1,18 @@
+#ifndef BLINKY_HPP
+#define BLINKY_HPP
+
+#include <SFML/Graphics.hpp>
+
+#include "../ghost.hpp"
+
+class Blinky : public Ghost {
+  public:
+    Blinky(const sf::Vector2f &_position) : Ghost("Blinky", _position) {};
+
+  protected:
+    sf::Vector2i getChasePosition() override {
+        return pacman->getMapPosition();
+    }
+};
+
+#endif //BLINKY_HPP
