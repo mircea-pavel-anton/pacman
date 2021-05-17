9cc626a6de43fbf8224a55cc77ff5f1c93d04d75
Mircea Anton <mircea.anton99@yahoo.com>
2021-05-17 17:49:51 +0300

    Import the edible barrel

--- src/pacman/GameTile/__barrel__.hpp
+++ src/pacman/GameTile/__barrel__.hpp
@@ -1,5 +1,3 @@
 #include "game_tile.hpp"
 #include "Wall/wall.hpp"
-#include "Edible/edible.hpp"
-#include "Edible/Food/food.hpp"
-#include "Edible/Fruit/fruit.hpp"
+#include "Edible/__barrel__.hpp"
