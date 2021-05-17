3c83db3a110c9ea49aca45203df493481e0df33f
Mircea Anton <mircea.anton99@yahoo.com>
2021-05-18 00:56:53 +0300

    Add all ghosts to the barrel file

--- src/pacman/GameTile/Movable/__barrel__.hpp
+++ src/pacman/GameTile/Movable/__barrel__.hpp
@@ -1,2 +1,6 @@
 #include "movable.hpp"
 #include "Pacman/pacman.hpp"
+#include "Ghost/ghost.hpp"
+#include "Ghost/Blinky/blinky.hpp"
+#include "Ghost/Pinky/pinky.hpp"
+#include "Ghost/Clyde/clyde.hpp"
