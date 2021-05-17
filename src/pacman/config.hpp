52f4d526d6e674f4d92b2920d769f5e0f91fd6ad
Mircea Anton <mircea.anton99@yahoo.com>
2021-05-17 16:33:09 +0300

    make all config values `static`.

--- src/pacman/config.hpp
+++ src/pacman/config.hpp
@@ -1,20 +1,20 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
-const int MAP_WIDTH = 28;       // game map width, in tiles.
-const int MAP_HEIGHT = 31;      // game map height, in tiles.
-const float TILE_SIZE = 22;     // tile size, in pixels.
+static const int MAP_WIDTH = 28;       // game map width, in tiles.
+static const int MAP_HEIGHT = 31;      // game map height, in tiles.
+static const float TILE_SIZE = 22;     // tile size, in pixels.
 
-const int WINDOW_HEIGHT = 900;  // window height, in pixels.
-const int WINDOW_WIDTH = 1200;  // window widthm in pixels.
+static const int WINDOW_HEIGHT = 900;  // window height, in pixels.
+static const int WINDOW_WIDTH = 1200;  // window widthm in pixels.
 
 // offset values used to center the map on the window.
-const float X_OFFSET = ( WINDOW_WIDTH - MAP_WIDTH * TILE_SIZE ) / 2;
-const float Y_OFFSET = ( WINDOW_HEIGHT - MAP_HEIGHT * TILE_SIZE ) / 2;
+static const float X_OFFSET = ( WINDOW_WIDTH - MAP_WIDTH * TILE_SIZE ) / 2;
+static const float Y_OFFSET = ( WINDOW_HEIGHT - MAP_HEIGHT * TILE_SIZE ) / 2;
 
-const int WINDOW_FRAMERATE = 60; // window framerate, in Hz.
+static const int WINDOW_FRAMERATE = 60; // window framerate, in Hz.
 
 // Movement speed, in pixels/frame.
-const float SPEED = 1;
+static const float SPEED = 1;
 
 #endif //CONFIG_HPP
