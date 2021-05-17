bbb719e98d03e5c9b6eeba0f7f91e7eab23b5142
Mircea Anton <mircea.anton99@yahoo.com>
2021-05-17 17:48:43 +0300

    Implement `PowerPellet` tile

---
+++ src/pacman/GameTile/Edible/PowerPellet/power_pellet.cpp
@@ -0,0 +1,13 @@
+#include "power_pellet.hpp"
+
+PowerPellet::PowerPellet(const sf::Vector2f &_position) {
+    initVars();
+    position = _position;
+
+    // PowerPellets are not animated.
+    // They have the same texture every frame.
+    texture_paths = { textures_root_dir + "power_pellet.png" };
+    score_modifier = 0;
+    
+    initSprite();
+}
---
+++ src/pacman/GameTile/Edible/PowerPellet/power_pellet.hpp
@@ -0,0 +1,15 @@
+#ifndef POWER_PELLET_HPP
+#define POWER_PELLET_HPP
+
+#include <SFML/Graphics.hpp>
+
+#include "edible.hpp"
+
+class PowerPellet : public Edible {
+
+  public:
+    PowerPellet(const sf::Vector2f &);
+
+}; //class PowerPellet
+
+#endif //POWER_PELLET_HPP
