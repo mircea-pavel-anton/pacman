d08ba43d51604bcf92b1fd02ddc1da1bf3881aa2
Mircea Anton <mircea.anton99@yahoo.com>
2021-05-18 00:56:40 +0300

    Implement `Clyde` ghost

---
+++ src/pacman/GameTile/Movable/Ghost/Clyde/clyde.hpp
@@ -0,0 +1,21 @@
+#ifndef CLYDE_HPP
+#define CLYDE_HPP
+
+#include <SFML/Graphics.hpp>
+
+#include "../ghost.hpp"
+
+class Clyde : public Ghost {
+  public:
+    Clyde(const sf::Vector2f &_position) : Ghost("Clyde", _position) {};
+
+  protected:
+    sf::Vector2i getChasePosition() override {
+        const sf::Vector2i delta = pacman->getMapPosition() - getMapPosition();
+        const int distance = delta.x * delta.x + delta.y * delta.y;
+        if (distance >= 64) return pacman->getMapPosition();
+        return {0, 0};
+    }
+};
+
+#endif //CLYDE_HPP
