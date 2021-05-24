b869643f52a563249895d4ea50b3241b3e84730f
Mircea Anton <mircea.anton99@yahoo.com>
2021-05-24 14:37:01 +0300

    Remove variable initializations

--- src/pacman/config.hpp
+++ src/pacman/config.hpp
@@ -14,19 +14,19 @@ typedef vector<vector<GameTile*>> vec2pGT;
 typedef vector<GameTile*> vec1pGT;
 
 // Map params.
-static int MAP_WIDTH = 28;       // game map width, in tiles.
-static int MAP_HEIGHT = 31;      // game map height, in tiles.
-static const float TILE_SIZE = 22;     // tile size, in pixels.
+static int MAP_WIDTH;               // game map width, in tiles.
+static int MAP_HEIGHT;              // game map height, in tiles.
+static const float TILE_SIZE = 22;  // tile size, in pixels.
 
 // Window params.
-static int WINDOW_HEIGHT = 900;  // window height, in pixels.
-static int WINDOW_WIDTH = 1200;  // window widthm in pixels.
+static int WINDOW_HEIGHT;               // window height, in pixels.
+static int WINDOW_WIDTH;                // window widthm in pixels.
 static const int WINDOW_FRAMERATE = 60; // window framerate, in Hz.
 static const std::string WINDOW_TITLE = "Pacman";
 
-// offset values used to center the map on the window.
-static const float X_OFFSET = ( WINDOW_WIDTH - MAP_WIDTH * TILE_SIZE ) / 2;
-static const float Y_OFFSET = ( WINDOW_HEIGHT - MAP_HEIGHT * TILE_SIZE ) / 2;
+// Offset values used to center the map on the window.
+static float X_OFFSET = 250;
+static float Y_OFFSET = 75;
 
 // Movement speed, in pixels/frame.
 static const float SPEED = 1;
@@ -34,7 +34,7 @@ static const float SPEED = 1;
 // Default durations for ghost states.
 const int SCARED_DURATION = 5 * WINDOW_FRAMERATE / TILE_SIZE;
 const int CHASE_DURATION = 30 * WINDOW_FRAMERATE / TILE_SIZE;
-const int SCATTER_DURATION = 5 * WINDOW_FRAMERATE / TILE_SIZE;
+const int SCATTER_DURATION = 10 * WINDOW_FRAMERATE / TILE_SIZE;
 const int HURT_DURATION = 2 * WINDOW_FRAMERATE / TILE_SIZE;
 
 static const std::string EMPTY_TEXTURE = "res/sprites/empty.png";
