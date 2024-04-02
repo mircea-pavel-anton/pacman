e523220e81d69f65306759c9785503a40b129412
Davis Tibbz <davisinyo@gmail.com>
2024-04-02 22:44:39 +0800

    format using clang-format

--- apple_cmake/MacBundle.cmake
+++ apple_cmake/MacBundle.cmake
@@ -5,42 +5,26 @@ set_source_files_properties(${application_icon}
                             PROPERTIES MACOSX_PACKAGE_LOCATION "Resources")
 
 # images
-file(GLOB_RECURSE my_images "${CMAKE_SOURCE_DIR}/res/*")
-foreach(FILE ${my_images})
-#  message(STATUS ${FILE})
-  get_filename_component(FILENAME ${FILE} NAME)
-  if(NOT FILENAME STREQUAL ".DS_Store")
-    # skip .DS_STORE
-    file(RELATIVE_PATH NEW_FILE "${CMAKE_SOURCE_DIR}/" ${FILE})
-    get_filename_component(REL_DIR ${NEW_FILE} DIRECTORY)
-    get_filename_component(END_FILE ${NEW_FILE} NAME)
-     if(DIR)
-            file(MAKE_DIRECTORY "${DEST}/${DIR}")
-        endif()
-     message(STATUS "RELATIVE DIR ${REL_DIR} END_FILE ${END_FILE}" )
-    set_source_files_properties(${FILE} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources/${REL_DIR}/${END_NAME}")
-  endif()
-endforeach()
+# file(GLOB_RECURSE my_images "${CMAKE_SOURCE_DIR}/res/*")
+# foreach(FILE ${my_images})
+# #  message(STATUS ${FILE})
+#   get_filename_component(FILENAME ${FILE} NAME)
+#   if(NOT FILENAME STREQUAL ".DS_Store")
+#     # skip .DS_STORE
+#     file(RELATIVE_PATH NEW_FILE "${CMAKE_SOURCE_DIR}/" ${FILE})
+#     get_filename_component(REL_DIR ${NEW_FILE} DIRECTORY)
+#     get_filename_component(END_FILE ${NEW_FILE} NAME)
+#      if(DIR)
+#             file(MAKE_DIRECTORY "${DEST}/${DIR}")
+#         endif()
+#      message(STATUS "RELATIVE DIR ${REL_DIR} END_FILE ${END_FILE}" )
+#     set_source_files_properties(${FILE} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources/${REL_DIR}/${END_NAME}")
+#   endif()
+# endforeach()
 
-function(copy_resources SOURCE DEST)
-    file(GLOB_RECURSE FILES "${SOURCE}/*")
-    foreach(FILE ${FILES})
-        file(RELATIVE_PATH REL_FILE ${SOURCE} ${FILE})
-        get_filename_component(DIR ${REL_FILE} DIRECTORY)
-        set(DEST_FILE "${DEST}/${REL_FILE}")
-        if(DIR)
-            file(MAKE_DIRECTORY "${DEST}/${DIR}")
-        endif()
-        add_custom_command(
-            OUTPUT "${DEST_FILE}"
-            COMMAND ${CMAKE_COMMAND} -E copy "${FILE}" "${DEST_FILE}"
-            DEPENDS "${FILE}"
-        )
-    endforeach()
-endfunction()
 
 add_executable(${CMAKE_PROJECT_NAME} MACOSX_BUNDLE
-               ${GAME_SRC} ${application_icon} "${my_images}")
+               ${GAME_SRC} ${application_icon} ${CMAKE_SOURCE_DIR}/res/)
 
 set_target_properties(
   ${CMAKE_PROJECT_NAME}
@@ -54,4 +38,4 @@ set_target_properties(
              MACOSX_BUNDLE_COPYRIGHT "(c) 2022, mirceanton"
              MACOSX_BUNDLE_BUNDLE_VERSION ${PROJECT_VERSION}
              MACOSX_BUNDLE_SHORT_VERSION_STRING ${PROJECT_VERSION}
-             RESOURCE "${my_images}")
+           RESOURCE ${CMAKE_SOURCE_DIR}/res/ )
--- src/pacman/GameTile/game_tile.cpp
+++ src/pacman/GameTile/game_tile.cpp
@@ -1,67 +1,63 @@
 #include "game_tile.hpp"
 
-GameTile::GameTile() { /* nothing to do here */ }
-GameTile::~GameTile() { /* nothing to do here */ }
+GameTile::GameTile() { /* nothing to do here */
+}
+GameTile::~GameTile() { /* nothing to do here */
+}
 
 void GameTile::initVars() {
-    // @sprite has it's own init method.
-    // @position should be set from the child class constructor.
-    // @texture_paths should be set from the child class constructor.
-    // @textures are loaded in the @initSprite() method.
-    frame_counter = 0;
+  // @sprite has it's own init method.
+  // @position should be set from the child class constructor.
+  // @texture_paths should be set from the child class constructor.
+  // @textures are loaded in the @initSprite() method.
+  frame_counter = 0;
 }
 
 void GameTile::initSprite() {
-    sprite = sf::Sprite();
+  sprite = sf::Sprite();
 
-    loadTextures();
+  loadTextures();
 
-    // Set on-screen position.
-    sprite.setPosition(position);
+  // Set on-screen position.
+  sprite.setPosition(position);
 }
 
 void GameTile::loadTextures() {
-    // Make sure vector is empty, just in case.
-    if (textures.empty() == false) textures.clear();
+  // Make sure vector is empty, just in case.
+  if (textures.empty() == false) textures.clear();
 
-    // Load each texture into the animation vector.
-    Config *config = Config::getInstance();
-    for (std::string &name : texture_names) {
-        textures.push_back( config->textures[name] );
-    }
+  // Load each texture into the animation vector.
+  Config *config = Config::getInstance();
+  for (std::string &name : texture_names) {
+    textures.push_back(config->textures[name]);
+  }
 }
 
 void GameTile::updateSprite() {
-    
+  // Load the texture from the animation vector
+  const int animation_index = (frame_counter / animation_fps) % textures.size();
+  sprite.setTexture(*(textures[animation_index]));
 
-    // Load the texture from the animation vector
-    const int animation_index = (frame_counter / animation_fps) % textures.size();
-    sprite.setTexture( *(textures[ animation_index ]) );
+  // Frame counter only goes from 0 to WINDOW_FRAMERATE - 1
+  frame_counter = (frame_counter + 1) % 60;
 
-    // Frame counter only goes from 0 to WINDOW_FRAMERATE - 1
-    frame_counter = (frame_counter + 1) % 60;
-
-    // Update on-screen position
-    sprite.setPosition({
-        position.x + Config::getInstance()->offset.x,
-        position.y + Config::getInstance()->offset.y,
-    });
-
-    
+  // Update on-screen position
+  sprite.setPosition({
+      position.x + Config::getInstance()->offset.x,
+      position.y + Config::getInstance()->offset.y,
+  });
 }
 
 void GameTile::render(sf::RenderTarget *_target) const {
-    
-    _target->draw(sprite);
-    
+  _target->draw(sprite);
 }
 
 GameTile &GameTile::operator=(const GameTile &_other) {
-    position = _other.position;
-    texture_names = _other.texture_names;
-    textures = _other.textures;
-    frame_counter = _other.frame_counter;
-    sprite = _other.sprite;
+  position = _other.position;
+  texture_names = _other.texture_names;
+  textures = _other.textures;
+  frame_counter = _other.frame_counter;
+  sprite = _other.sprite;
 
-    return *this;
+  return *this;
 }
